d7d93c8dce4c9bfff8e6fae27d3abd4408019516
ken0901 <ken5260715@gmail.com>
2021-09-08 00:59:25 -0700

    commit test

--- src/main/java/com/ken/blog/BlogApplication.java
+++ src/main/java/com/ken/blog/BlogApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BlogApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class,args);
+        System.out.println("test");
     }
 }
