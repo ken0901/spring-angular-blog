1c963d1cc4319c648d2c9aeaad8a66ab34fe0837
ken0901 <ken5260715@gmail.com>
2021-09-14 22:33:37 -0700

    Add Asymmetric Encryption using Java keystore

--- src/main/java/com/ken/blog/security/JwtProvider.java
+++ src/main/java/com/ken/blog/security/JwtProvider.java
@@ -1,57 +1,91 @@
 package com.ken.blog.security;
 
+import com.ken.blog.exception.SpringBlogException;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.security.Key;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.*;
+import java.security.cert.CertificateException;
 
 @Service
 public class JwtProvider {
 
-    private Key key;
+    private KeyStore keyStore;
 
+    /**
+     * Asymmetric Encryption using Java keystore
+     * keytool -genkey -alias springblog -keyalg RSA -keystore springblog.jks -keysize 2048
+     */
     @PostConstruct
-    public void init(){
-        key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    public void init() {
+        try {
+            keyStore = KeyStore.getInstance("JKS");
+            InputStream resourceAsStream = getClass().getResourceAsStream("/springblog.jks");
+            keyStore.load(resourceAsStream, "777777".toCharArray());
+        } catch (KeyStoreException | CertificateException | NoSuchAlgorithmException | IOException e) {
+            throw new SpringBlogException("Exception occurred while loading keystore");
+        }
+
     }
 
     /**
-     *
      * @param authentication
      * @return JWT token
      */
-    public String generateToken(Authentication authentication){
+    public String generateToken(Authentication authentication) {
         User principal = (User) authentication.getPrincipal();
         return Jwts.builder()
                 .setSubject(principal.getUsername())
-                .signWith(key)
+                .signWith(getPrivateKey())
                 .compact();
     }
 
     /**
-     *
+     * @return Key
+     * @throws SpringBlogException
+     */
+    private Key getPrivateKey() {
+        try {
+            return (PrivateKey) keyStore.getKey("springblog", "777777".toCharArray());
+        } catch (KeyStoreException | NoSuchAlgorithmException | UnrecoverableKeyException e) {
+            throw new SpringBlogException("Exception occurred while retrieving public key from keystore");
+        }
+    }
+
+    /**
      * @param jwt token
      * @return boolean true
      */
-    public boolean validateToken(String jwt){
-        Jwts.parser().setSigningKey(key).parseClaimsJws(jwt);
+    public boolean validateToken(String jwt) {
+        Jwts.parser().setSigningKey(getPublicKey()).parseClaimsJws(jwt);
         return true;
     }
 
     /**
-     *
+     * @return PublicKey
+     * @throws SpringBlogException
+     */
+    private PublicKey getPublicKey() {
+        try {
+            return keyStore.getCertificate("springblog").getPublicKey();
+        } catch (KeyStoreException e) {
+            throw new SpringBlogException("Exception occurred while retrieving public key from keystore");
+        }
+    }
+
+    /**
      * @param jwt token
      * @return
      */
     public String getUsernameFromJWT(String jwt) {
         Claims claims = Jwts.parser()
-                .setSigningKey(key)
+                .setSigningKey(getPublicKey())
                 .parseClaimsJws(jwt)
                 .getBody();
 
